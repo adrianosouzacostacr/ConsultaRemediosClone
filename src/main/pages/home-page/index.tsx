0806b238466723ce70502075b149bfa7217327f1
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-11-09 13:35:18 -0300

    Refactor env structure

--- src/main/pages/home-page/index.tsx
+++ src/main/pages/home-page/index.tsx
@@ -5,12 +5,14 @@ import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
 import env from './../../../../env';
 
 const App = () => {
+  const {product_name} = env;
+
   return (
     <>
       <StatusBar />
       <SafeAreaView style={styles.safeArea}>
         <Text accessibilityLabel="brand" testID="">
-          {env.product_name}
+          {product_name}
         </Text>
         <Text accessibilityLabel="message" testID="">
           Agora sim vai dar certo \o/
